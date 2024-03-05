abb1ec6ffbf4c2f781d7f47ee0c2b6d31295ec6e
Jax-Hax <jaxhacks543@gmail.com>
2024-03-05 12:59:10 -0500

    spawned them in correct position and used a signal

--- PipeSpawner.gd
+++ PipeSpawner.gd
@@ -1,9 +1,14 @@
 extends Node
 
-var scene = preload("res://my_scene.tscn")
+var scene : PackedScene = preload("res://pipes.tscn")
 
 func _ready():
 	spawn_pipes()
 	
 func spawn_pipes():
-	
+	var instance = scene.instantiate()
+	instance.global_position.x = 1250.0
+	add_child(instance)
+
+func _on_timer_timeout():
+	spawn_pipes()
